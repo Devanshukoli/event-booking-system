2f714d5d96fe26d04a2f1de0b4e6189f3412756e
Devanshukoli <kolidevanshu02@gmail.com>
2026-05-11 16:46:50 +0530

    update server.js file

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ import cors from 'cors';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import authRoutes from './routes/auth.js';
+import eventRoutes from './routes/events.js';
+import bookingRoutes from './routes/bookings.js';
 import errorHandler from './middleware/errorHandler.js';
+import swaggerUi from 'swagger-ui-express';
+import swaggerSpec from './swagger.js';
 
 dotenv.config();
 
@@ -15,6 +20,10 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
+app.use('/api/auth', authRoutes);
+app.use('/api/events', eventRoutes);
+app.use('/api/bookings', bookingRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.get('/', (req, res) => {
   res.json({ message: 'Event Booking System API is running.' });
